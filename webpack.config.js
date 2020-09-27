b7e55b8fe8c73f905327e6170114087fe24783d7
san4oys96 <hitman10091996@gmail.com>
2020-09-27 13:20:45 +0300

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const miniCss = require('mini-css-extract-plugin');
-
 module.exports = {
 	entry: './src/index.js',
 	output: {
@@ -15,7 +14,15 @@ module.exports = {
 				'css-loader',
 				'sass-loader',
 			]
-		}]
+		},
+		{
+			test: /\.(png|jpe?g|gif)$/i,
+			use: [
+				{
+					loader: 'file-loader',
+				},
+			],
+		},]
 	},
 	plugins: [
 		new miniCss({
